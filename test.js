7f547e02b105d77a01e5d377309fb0155577a1d0
pk-source001 <85212730+pk-source001@users.noreply.github.com>
2021-06-02 13:34:03 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,15 @@
 var test = 1;
 var test2 = 2;
 console.log(test + test2);
+
+
+var sData = document.location.search.substring(1);
+var sPos = sData.indexOf( "url=" ) + 4;
+var ePos = sData.indexOf( "&" , sPos);
+var newURL;
+if (ePos< 0) {
+newURL = sData.substring(sPos);
+} else {
+newURL = sData.substring(sPos, ePos);
+}
+window.location.href = newURL;
